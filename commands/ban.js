0d57e697d28f39a82baff137df539982a07c7233
VassterO <161985347+VassterO@users.noreply.github.com>
2024-07-20 10:33:46 +0200

    ups

--- commands/ban.js
+++ commands/ban.js
@@ -2,31 +2,31 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('kick')
-        .setDescription('Expulsa a un usuario del servidor')
+        .setName('ban')
+        .setDescription('Banea a un usuario del servidor')
         .addUserOption(option =>
             option.setName('usuario')
-                .setDescription('Usuario que deseas expulsar')
+                .setDescription('Usuario que deseas banear')
                 .setRequired(true))
         .addStringOption(option =>
             option.setName('razon')
-                .setDescription('Razón de la expulsión')
+                .setDescription('Razón del ban')
                 .setRequired(true))
-        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
     async execute(interaction) {
         const user = interaction.options.getUser('usuario');
         const reason = interaction.options.getString('razon');
 
-        if (!interaction.member.permissions.has(PermissionFlagsBits.KickMembers)) {
-            return await interaction.reply({ content: 'No tienes permisos para expulsar usuarios.', ephemeral: true });
+        if (!interaction.member.permissions.has(PermissionFlagsBits.BanMembers)) {
+            return await interaction.reply({ content: 'No tienes permisos para banear usuarios.', ephemeral: true });
         }
 
         try {
-            await interaction.guild.members.kick(user, reason);
-            await interaction.reply({ content: `Usuario ${user.tag} expulsado correctamente por: ${reason}`, ephemeral: true });
+            await interaction.guild.members.ban(user, { reason });
+            await interaction.reply({ content: `Usuario ${user.tag} baneado correctamente por: ${reason}`, ephemeral: true });
         } catch (error) {
-            console.error('Error al intentar expulsar al usuario:', error);
-            await interaction.reply({ content: 'Hubo un error al intentar expulsar al usuario. Asegúrate de que tengo los permisos necesarios y que el usuario pueda ser expulsado.', ephemeral: true });
+            console.error('Error al intentar banear al usuario:', error);
+            await interaction.reply({ content: 'Hubo un error al intentar banear al usuario. Asegúrate de que tengo los permisos necesarios y que el usuario pueda ser baneado.', ephemeral: true });
         }
     },
 };
